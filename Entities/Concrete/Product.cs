aebb23353d9a58fa8e3f445649963ccc3f156003
BERKAY ŞAVKLI <berkaysavkli@gmail.com>
2021-01-31 00:19:27 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product
+        //class ı public yaptığımız zaman diğer katmanlardan erişilebilmesini sağlar
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock  { get; set; }
+        public decimal UnitPrice  { get; set; }
+
+    }
+    
+}
