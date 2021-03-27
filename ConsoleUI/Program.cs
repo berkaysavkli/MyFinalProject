98928a70a1be166b936c14f4dc35aca718ba4f7c
BERKAY ŞAVKLI <berkaysavkli@gmail.com>
2021-03-27 23:46:58 +0300

    27.03.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             //EfProductDal yerine InMemoryProductDal yazarsak memeory de çalışmış oluruz.
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAllByCategoryId(2))
-                // productManager. dan sonraki kısmı değiştirerek  yazdığımız farklı filtremelere
-                //göre verileri getiririz.
-             
+            foreach (var product in productManager.GetProductDetails())
+            // productManager. dan sonraki kısmı değiştirerek  yazdığımız farklı filtremelere
+            //göre verileri getiririz.
+
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
             }
         }
     }
