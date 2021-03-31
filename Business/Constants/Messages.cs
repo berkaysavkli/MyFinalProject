f21f4fa37cac62ffe20e31d0f4b6be1ddd4e7499
BERKAY ŞAVKLI <berkaysavkli@gmail.com>
2021-03-31 17:13:27 +0300

    31.03.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+   public static class Messages
+    {
+        //Temel mesajları buraya yazıyoruz.
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+    }
+}
