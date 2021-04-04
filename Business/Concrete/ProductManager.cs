135e1cd1417c9762002db357e25cf34c072ff26a
BERKAY ŞAVKLI <berkaysavkli@gmail.com>
2021-04-04 22:51:27 +0300

    04.04.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Apects.Autofac.Validation;
+using Core.CrossCuttingCencern.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,13 +23,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-           // Ürünü eklemeden önce gerçekleşmesi gereken kurallar varsa buraya yazılır.
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            // Ürünü eklemeden önce gerçekleşmesi gereken kurallar varsa buraya yazılır.
+            
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
@@ -38,7 +40,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==14)
+            if (DateTime.Now.Hour==22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            //Categori 1 olan ürünlerin fiyatı en az 10 olabilir demek.
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p=>p.CategoryId == 1);
+            //FluentValidation da olmayan bir kural yazmak istersek aşağıdaki gibi yazılır.
+            //Örneğin bütün ürünler a ile başlamalı  diye bir kral yazarsak
+            //StartWithA 'yi biz yazdık.
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
